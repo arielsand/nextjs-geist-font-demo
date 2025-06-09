f7056aa2f978d0eb8d6f4a73d224de0865235268
Ariel Sand <ariel.sand@gmail.com>
2025-06-09 10:04:03 +0200

    Add .next/static/chunks/src_app_builder_page_tsx_9e374265._.js 1749456243052

---
+++ .next/static/chunks/src_app_builder_page_tsx_9e374265._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_8455f2f2._.js",
+  "static/chunks/node_modules_16900a51._.js"
+],
+    source: "dynamic"
+});
